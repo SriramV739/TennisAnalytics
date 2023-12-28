f3b82ed60510a266d6aec7a220578f7345e57d59
SriramV739 <sriramvenkatesh739@gmail.com>
2023-12-28 10:29:42 -0800

    a

--- analysis.py
+++ analysis.py
@@ -8,6 +8,7 @@ from matplotlib.ticker import MultipleLocator
 import os
 import sys
 from datetime import datetime, timedelta
+import csv
 
 # Read CSV File
 df=pd.read_csv('/Users/sriram/Downloads/tennisanalytics/data/atp_matches_2022.csv')
@@ -20,7 +21,35 @@ fout=open("winners.out","w")
 #w_SvGms,w_bpSaved,w_bpFaced,l_ace,l_df,l_svpt,l_1stIn,l_1stWon,l_2ndWon,l_SvGms,l_bpSaved,l_bpFaced,winner_rank,
 #winner_rank_points,loser_rank,loser_rank_points
 
-df1=df[df['round']=='F']
-subset=['tourney_name','winner_name','loser_name']
-df1=df1[subset]
-df1.to_csv('winners.csv')
+players=[]
+for i in df['winner_name'].items():
+    players.append(i[1])
+
+players=list(set(players))
+def print_winners():
+    df1=df[df['round']=='F']
+    subset=['tourney_name','winner_name','loser_name']
+    df1=df1[subset]
+    df1.to_csv('winners.csv',index=False)
+
+print_winners()
+def best_record(min_games):
+    record={}
+    for i in players:
+        wins=df[df['winner_name']==i].shape[0]
+        loss=df[df['loser_name']==i].shape[0]
+        if wins+loss>=min_games:
+            record[i]=float(wins)/(wins+loss)
+            
+    res=[[]]
+    record=dict(sorted(record.items(), key=lambda item: item[1],reverse=True))
+    for i in record.keys():
+        res.append([i,record[i]])
+    with open("/Users/sriram/Downloads/tennisanalytics/results/record.csv", mode='w', newline='') as file:
+        writer = csv.writer(file)
+        writer.writerows(res)
+    him=list(record.keys())[0]
+    print(him,record[him])
+
+
+best_record(10)
