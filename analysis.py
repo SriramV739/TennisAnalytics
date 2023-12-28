5e554e86ce0c0e080f349c688b6db8d4275b25de
SriramV739 <sriramvenkatesh739@gmail.com>
2023-12-28 11:07:40 -0800

    a

--- analysis.py
+++ analysis.py
@@ -11,9 +11,8 @@ from datetime import datetime, timedelta
 import csv
 
 # Read CSV File
-df=pd.read_csv('/Users/sriram/Downloads/tennisanalytics/data/atp_matches_2022.csv')
-
-fout=open("winners.out","w")
+year='2022'
+df=pd.read_csv(f'/Users/sriram/Downloads/tennisanalytics/data/atp_matches_{year}.csv')
 
 #headers:tourney_id,tourney_name,surface,draw_size,tourney_level,tourney_date,match_num,winner_id,
 #winner_seed,winner_entry,winner_name,winner_hand,winner_ht,winner_ioc,winner_age,loser_id,loser_seed,loser_entry,
@@ -25,12 +24,21 @@ players=[]
 for i in df['winner_name'].items():
     players.append(i[1])
 
+for i in df['loser_name'].items():
+    players.append(i[1])
+def writefile(arr,filepath):
+    with open(filepath, mode='w', newline='') as file:
+        writer = csv.writer(file)
+        writer.writerows(arr)
+        
+        
+        
 players=list(set(players))
 def print_winners():
     df1=df[df['round']=='F']
     subset=['tourney_name','winner_name','loser_name']
     df1=df1[subset]
-    df1.to_csv('winners.csv',index=False)
+    df1.to_csv('results/winners.csv',index=False)
 
 print_winners()
 def best_record(min_games):
@@ -41,15 +49,22 @@ def best_record(min_games):
         if wins+loss>=min_games:
             record[i]=float(wins)/(wins+loss)
             
-    res=[[]]
+    res=[]
     record=dict(sorted(record.items(), key=lambda item: item[1],reverse=True))
     for i in record.keys():
         res.append([i,record[i]])
-    with open("/Users/sriram/Downloads/tennisanalytics/results/record.csv", mode='w', newline='') as file:
-        writer = csv.writer(file)
-        writer.writerows(res)
     him=list(record.keys())[0]
     print(him,record[him])
 
 
-best_record(10)
+#best_record(10)
+def no_5sets():
+    filtered_df = df[df['score'].apply(lambda x: len(str(x).split())) == 5]
+    #filtered_df=df[len(list(str(df['score']).split()))==5]
+    res=[]
+    for i in players:
+        filtered_df1=filtered_df[(filtered_df['winner_name']==i)|(filtered_df['loser_name']==i)]
+        res.append([i,filtered_df1.shape[0]])
+    res = sorted(res, key=lambda x: x[1],reverse=True)
+    writefile(res,"/Users/sriram/Downloads/tennisanalytics/results/5sets.csv")
+no_5sets()
